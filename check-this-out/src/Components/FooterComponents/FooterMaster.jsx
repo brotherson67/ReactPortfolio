3620fa25a84f9c1c2f447369e4936c288b5b5c04
brotherson67 <mitchell.brotherson@gmail.com>
2022-01-30 14:56:54 -0700

    got footer to be how I want it

--- check-this-out/src/Components/FooterComponents/FooterMaster.jsx
+++ check-this-out/src/Components/FooterComponents/FooterMaster.jsx
@@ -1,12 +1,14 @@
-import "./FooterMaster";
+import "./FooterMaster.css";
 import logo from "../../assets/images/GitHub-Mark/PNG/GitHub-Mark-32px.png";
 
 function FooterMaster() {
   return (
-    <footer id="footer">
-      <img src={logo} alt="logo" />
-      <h2>Github</h2>
-    </footer>
+    <div className="footer">
+      <a href="https://github.com/brotherson67">
+        <img src={logo} alt="logo" />
+        <h2>Github</h2>
+      </a>
+    </div>
   );
 }
 
